bebb238c83dce8f44de672355994f4f934cb91e3
reDan22 <daniilfrolov5@gmail.com>
2022-10-13 20:25:13 +0300

    Modified

--- 1.py
+++ 1.py
@@ -1,25 +1,26 @@
 import random
 
+try:
+    print("Введите количество элементов массива:")
+    kolvo_elementov = int(input())
 
-print("Введите размерность массива:")
-kolvo_elementov = int(input())
+    array = [0] * kolvo_elementov
+    max_elenent = 0
 
-array = [0] * kolvo_elementov
-max_elenent = 0
+    for i in range(kolvo_elementov):
+        array[i] = random.randint(100,10000) / 100
+    print("Изначальный массив:")
+    print(array)
 
-for i in range(kolvo_elementov):
-    array[i] = random.randint(100,10000) / 100
-print("Изначальный массив:")
-print(array)
+    for j in range(kolvo_elementov):
+        if array[j] > max_elenent:
+            max_elenent = array[j]
+            element_number = j
+    for a in range(element_number + 1, kolvo_elementov):
+        array[a] = 0
 
-for j in range(kolvo_elementov):
-    if array[j] > max_elenent:
-        max_elenent = array[j]
-        element_number = j
-print(element_number, max_elenent)
-for a in range(element_number + 1, kolvo_elementov):
-    array[a] = 0
-
-print("Изменённый массив:")
-print(array)
-    
+    print("Изменённый массив:")
+    print(array)
+except ValueError:
+    print("Введите целое число в качестве количества эллементов массива")
+    quit()
